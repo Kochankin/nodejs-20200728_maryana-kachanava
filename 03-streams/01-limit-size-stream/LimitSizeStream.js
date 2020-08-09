0456c3dd1d3b0aaf7483abd88e7a69dc6cabb3ca
Marianna <marian110791@mail.ru>
2020-08-09 17:12:34 +0300

    01-limit-size-streams | add limit check

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,23 +1,17 @@
-const stream = require('stream');
-const LimitExceededError = require('./LimitExceededError');
+const stream = require("stream");
+const LimitExceededError = require("./LimitExceededError");
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
-
-    this.limit = options.limit;
-    this.size = 0;
-    this.isObjectMode = !!options.readableObjectMode;
+    this.options = options;
+    this.len = 0;
   }
 
   _transform(chunk, encoding, callback) {
-    if (this.isObjectMode) {
-      this.size += 1;
-    } else {
-      this.size += chunk.length;
-    }
+    this.len += chunk.length;
 
-    if (this.size > this.limit) {
+    if (this.len > this.options.limit) {
       callback(new LimitExceededError());
     } else {
       callback(null, chunk);
